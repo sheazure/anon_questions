4dd8556fd5d8c368a222d905c4f19bf5e0ffec8c
sheazure <sheazuree@gmail.com>
2024-12-02 17:43:30 +0300

    adding comments and buttons

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from os.path import join, dirname
 
 
+
 def get_from_env(key):
     dotenv_path = join(dirname(__file__), "token.env")
     load_dotenv(dotenv_path)
@@ -15,48 +16,64 @@ def get_from_env(key):
 token = get_from_env('TG_BOT_TOKEN')
 bot = telebot.TeleBot(token)
 
-recip = None
-sender = None
-sender_username = None
-id_message = None
-event_cancel_send = False
+recip = None # получатель анонимки(владелец ссылки)
+sender = None # отправитель анонимки(человек, который перешел по ссылке)
+sender_username = None # его юз соответственно
+id_message = None # айди сообщения(бля не помню какого сообщения)
+event_cancel_send = False # событие отмены отправки(если соблюдается, закрываем все функции)
 
-
-previous_messages = []
+#id: [messages]
+history_messages = {}
 
 
 @bot.message_handler(commands=['start', 'hello'])
 def start(message):
     global recip, id_message, sender, sender_username, event_cancel_send
-    bot.delete_message(message.chat.id, message.id)
-    check_user(message) # добавленме в бд пользователей
+    bot.delete_message(message.chat.id, message.id) # удаление сообщения /start
+    check_user(message) # добавление в бд пользователей
+
+    # создание всех бд в директории
     create_all_databases()
 
 
-    if " " not in message.text: # не по рефу
+    if " " not in message.text: # не по рефу(человек хочет создать ссылку)
         link_user = 'https://t.me/AskYourFriendAnon_bot/?start=' + str(for_ref(message.from_user.id, int(key, 2)))
         bot.send_message(message.chat.id, f"🚀Начни получать анонимные сообщения прямо сейчас!\n\nВот твоя ссылка:\n👉 {link_user}\n\nРазмести эту ссылку в своих соц.сетях и начинай получать анонимные сообщения💬")
-    else: # по рефу
+    else: # по рефу (перешел по чьей-то ссылке)
         sender = message.from_user.id
         sender_username = message.from_user.username
-        recip = message.text.split()[1]
-        recip = for_ref(recip, int(key, 2)) # id получателя
 
-        markup = telebot.types.InlineKeyboardMarkup()
-        Button = telebot.types.InlineKeyboardButton("Отменить отправку", callback_data='cancel_send')
+        recip = message.text.split()[1] # получатель сообщения
+        recip = for_ref(recip, int(key, 2)) # id получателя(дешифруем id получателя)
 
-        markup.add(Button)
+        markup = telebot.types.InlineKeyboardMarkup() # кнопка
+        Button = telebot.types.InlineKeyboardButton("Отменить отправку", callback_data='cancel_send') # кнопка
+        markup.add(Button) 
 
         bot.send_message(message.chat.id, "Напиши сообщение, которое ты хочешь отправить, владелец ссылки получит его, но не будет знать от кого оно!", reply_markup=markup)
-        bot.register_next_step_handler(message, send_message)
+        if event_cancel_send:
+            event_cancel_send = False
+            return 
+        else:
+            bot.register_next_step_handler(message, send_message)
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     global event_cancel_send
+
     if callback.data == 'cancel_send':
         event_cancel_send = True
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        return
+    
+    if callback.data == 'send_more':
+        markup = telebot.types.InlineKeyboardMarkup() # кнопка
+        Button = telebot.types.InlineKeyboardButton("Отменить отправку", callback_data='cancel_send') # кнопка
+        markup.add(Button)
+
+        bot.send_message(callback.message.chat.id, "Напиши сообщение, которое ты хочешь отправить, владелец ссылки получит его, но не будет знать от кого оно!", reply_markup=markup)
+        bot.register_next_step_handler(callback.message, send_message)
 
         
 
@@ -133,25 +150,61 @@ def show_ias(message):
 
 
 
+@bot.message_handler(commands=['add_admin'])
+def add_admin(message):
+    if not check_admin(message):
+        bot.reply_to(message, "Неизвестная команда!")
+        return
+    
+    bot.send_message(message.chat.id, "Введи id и username пользователя через пробел")
+    bot.register_next_step_handler(message, add_admin_step2)
+
+def add_admin_step2(message):
+    user_data = message.text.split()
+    if len(user_data) < 2:
+        bot.send_message(message.chat.id, "Введены некорректные данные! Попробуйте еще раз")
+        return
+
+    conn = sqlite3.connect(r'admins.sql')
+    cur = conn.cursor()
+    try:
+        cur.execute("INSERT INTO admins (id, username) VALUES ('%d', '%s')" % (int(user_data[0]), user_data[1]))
+        conn.commit()
+    except ValueError:
+        bot.send_message(message.chat.id, "Введены некорректные данные! Попробуйте еще раз")
+        bot.register_next_step_handler(message, add_admin_step2)
+
+    cur.close()
+    conn.close()
+    bot.send_message(message.chat.id, "Админ успешно добавлен!")
+
+
 def send_message(message):
     global id_message, sender, recip, sender_username, event_cancel_send
-    if event_cancel_send:
+    if event_cancel_send: # если отменили отправку return 
         event_cancel_send = False
         return
     text = message.text
 
     if text[0] == '/':
-        bot.send_message(message.chat.id, "Неизвестная команда")
+        bot.send_message(message.chat.id, "Неизвестная команда") # ибо она не сработала во всех хендлерах
         bot.register_next_step_handler(message, send_message)
         return
 
-    
-    bot.reply_to(message, "Сообщение отправлено! Ожидай ответа от получателя")
-    bot.delete_message(message.chat.id, message.id - 1)
-    link_user = 'https://t.me/AskYourFriendAnon_bot/?start=' + str(for_ref(message.from_user.id, int(key, 2)))
-    bot.send_message(message.chat.id, f"🚀Начни получать анонимные сообщения прямо сейчас!\n\nВот твоя ссылка:\n👉 {link_user}\n\nРазмести эту ссылку в своих соц.сетях и начинай получать анонимные сообщения💬")
+
     id_message = bot.send_message(recip, "📩Тебе пришло новое анонимное сообщение🚀\n\n" + text + "\n\nСвайпни для ответа↩️\n")
     id_message = id_message.message_id
+
+    markup = telebot.types.InlineKeyboardMarkup() # кнопка
+    Button = telebot.types.InlineKeyboardButton("Написать еще", callback_data='send_more') # кнопка
+    markup.add(Button) 
+    
+    bot.send_message(message.chat.id, "Сообщение отправлено! Ожидай ответа от получателя", reply_markup=markup)
+    bot.delete_message(message.chat.id, message.id - 1) # удаление сообщения, хз какого
+
+    link_user = 'https://t.me/AskYourFriendAnon_bot/?start=' + str(for_ref(message.from_user.id, int(key, 2)))
+    bot.send_message(message.chat.id, f"🚀Начни получать анонимные сообщения прямо сейчас!\n\nВот твоя ссылка:\n👉 {link_user}\n\nРазмести эту ссылку в своих соц.сетях и начинай получать анонимные сообщения💬")
+    
     
     #добавление в бд неотвеченных анонок
     conn = sqlite3.connect(r'ias.sql')
@@ -164,7 +217,7 @@ def send_message(message):
     cur.close()
     conn.close()
     
-    previous_messages.append(message)
+
 
 
 @bot.message_handler()
@@ -177,34 +230,35 @@ def get_answer(message):
         conn = sqlite3.connect(r'ias.sql')
         cur = conn.cursor()
 
-        info = cur.execute(f'SELECT * FROM ias WHERE id_owner={message.from_user.id}')
+        info = cur.execute(f'SELECT * FROM ias WHERE id_owner={message.from_user.id}') # выбираем все анонки, которые принадлежат пользователю(по айди)
 
         a = info.fetchall()
 
         for el in a:
-            if message.reply_to_message.message_id == el[2]:
+            if message.reply_to_message.message_id == el[2]: # если айди сообщения на которое ответили, есть в бд неотвеченных анонок(воизбежание ответа на другое сообщение бота)
                 bot.send_message(el[0], "Ответ отправлен!")
-                bot.send_message(el[3], "Тебе пришел ответ от человека, которому ты отправил анонимное сообщение:\n\n" + str(message.text))
+                bot.send_message(el[3], f"Тебе пришел ответ от человека, которому ты отправил анонимное сообщение:\n\n" + str(message.text))
                 
-                return 0
+                # el[5] текст который надо нам искать в диалоге с el[3]
+
+                return 5
         
         bot.send_message(message.from_user.id, "Ты можешь отвечать только на анонимные сообщения🤓🤓🤓")
         
         cur.close()
         conn.close()
-    else:
+    else: # если человек что-то написал, не указывая на анонку
         link_user = 'https://t.me/AskYourFriendAnon_bot/?start=' + str(for_ref(message.from_user.id, int(key, 2)))
         bot.send_message(message.chat.id, f"🚀Начни получать анонимные сообщения прямо сейчас!\n\nВот твоя ссылка:\n👉 {link_user}\n\nРазмести эту ссылку в своих соц.сетях и начинай получать анонимные сообщения💬")
 
 
 
-def check_admin(message):
+def check_admin(message): # проверка на админа
 
     if message.from_user.id in [1404205394]:
         return True
     else:
         return False
-
 
         
 def check_user(message): # добавление в список юзеров
